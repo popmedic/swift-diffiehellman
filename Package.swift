c296d4b8b36c20886cfe90eec63cf6fcb56f308a
Kevin Scardina <kscardina@gmail.com>
2021-02-28 22:11:00 -0700

    add package dependency AurgumentParser

--- Package.swift
+++ Package.swift
@@ -10,9 +10,16 @@ let package = Package(
             targets: [
                 "DiffieHellmanSecurity"
             ]
+        ),
+        .executable(
+            name: "dhdigest",
+            targets: ["dhdigest"]
         )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser",
+                 from: "0.2.0")
+    ],
     targets: [
         .target(
             name: "DiffieHellmanSecurity",
@@ -22,6 +29,16 @@ let package = Package(
             name: "DiffieHellmanSecurityTests",
             dependencies: [
                 "DiffieHellmanSecurity"
+            ]
+        ),
+        .target(
+            name: "dhdigest",
+            dependencies: [
+                "DiffieHellmanSecurity",
+                .product(
+                    name: "ArgumentParser",
+                    package: "swift-argument-parser"
+                )
             ]
         )
     ]
